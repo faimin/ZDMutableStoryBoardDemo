f1bdd114b0be37c2e011afbbb326bd8a9ad95b40
Zero.D.Saber <fuxianchao@gmail.com>
2016-09-24 00:48:18 +0800

    update

---
+++ ZDMutableStoryBoardDemo/Controller/Two/TwoController1.h
@@ -0,0 +1,13 @@
+//
+//  TwoController1.h
+//  ZDMutableStoryBoardDemo
+//
+//  Created by 符现超 on 16/9/24.
+//  Copyright © 2016年 Fate.D.Bourne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TwoController1 : UIViewController
+
+@end
---
+++ ZDMutableStoryBoardDemo/Controller/Two/TwoController2.m
@@ -0,0 +1,37 @@
+//
+//  TwoController2.m
+//  ZDMutableStoryBoardDemo
+//
+//  Created by 符现超 on 16/9/24.
+//  Copyright © 2016年 Fate.D.Bourne. All rights reserved.
+//
+
+#import "TwoController2.h"
+
+@interface TwoController2 ()
+
+@end
+
+@implementation TwoController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
